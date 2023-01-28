29c3f8264a9eabc17b1660b5777ffc5b02c8a7c9
Squall Leonhart <squallpl1983@gmail.com>
2023-01-27 20:06:04 -0500

    Consolidated lists into single item.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import { Box, CssBaseline, Link, Tabs, Tab } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-import ExperienceList from './ExperienceList';
-import EducationList from './EducationList';
-import ProjectList from './ProjectList';
+// import ExperienceList from './ExperienceList';
+// import EducationList from './EducationList';
+// import ProjectList from './ProjectList';
+import PortfolioItem from './PortfolioItem';
+import { projects, experiences, educations } from '../database';
 
 import Nav from './Nav';
 
@@ -25,7 +27,7 @@ const boxStyle = {
 export default () => {
   const [value, setValue] = useState(0);
 
-  const handleChange = (event, newValue) => {
+  function handleChange(event, newValue) {
     setValue(newValue);
   };
 
@@ -35,29 +37,31 @@ export default () => {
       <Nav />
       <Box sx={{ width: '100%', top: '69px', bgcolor: 'background.default', zIndex: 1 }} position="fixed">
         <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
-          <LinkTab label="Projects" href="#project-list" />
-          <LinkTab label="Technical Experience" href="#experience-list" />
           <LinkTab label="Education" href="#education-list" />
+          <LinkTab label="Technical Experience" href="#experience-list" />
+          <LinkTab label="Projects" href="#project-list" />
         </Tabs>
       </Box>
       {/* Hero Header */}
       <Box sx={boxStyle}>
-        <ProjectList />
-        <ExperienceList />
-        <EducationList />
+        <div id="education-list" className="portfolio-list">
+          {educations.map((education, index) => {
+            return <PortfolioItem key={index} detailObject={education}/>
+          })}
+        </div>
+        <div id="experience-list" className="portfolio-list">
+        {experiences.map((experience, index) => {
+            return <PortfolioItem key={index} detailObject={experience}/>
+          })}
+        </div>
+        <div id="project-list" className="portfolio-list">
+          {projects.map((project, index) => {
+            return <PortfolioItem key={index} detailObject={project}/>
+          })}
+        </div>
       </Box>
     </ThemeProvider>
   );
 }
 
-const LinkTab = (props) => {
-  return (
-    <Tab
-      component={Link}
-      // onClick={(event) => {
-      //   event.preventDefault();
-      // }}
-      {...props}
-    />
-  );
-}
+const LinkTab = (props) => <Tab component={Link} {...props}/>
---
+++ client/src/PortfolioItem.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Paper, Box, Button, ButtonGroup } from '@mui/material';
+
+// import { projects } from '../database';
+
+const linksBoxStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  '& > *': {
+    m: 1,
+  },
+};
+
+export default ({ detailObject }) => {
+
+  return (
+    <Paper elevation={3}>
+    <Box sx={{p: 2}}>
+      {!!detailObject?.title && <h3>{detailObject.title}</h3>}
+
+      {!!detailObject?.type && <p>{detailObject.type}</p>}
+
+      {!!detailObject?.company && <p>{detailObject.company}</p>}
+
+      {!!detailObject?.status && <p>{detailObject.status}</p>}
+
+      {(detailObject?.links?.length > 0) && <LinksBox links={detailObject.links} />}
+
+      {(detailObject?.tech_used?.length > 0) && <TechList list={detailObject.tech_used} />}
+
+      {!!detailObject?.description && <pre>{detailObject.description}</pre>}
+
+      {!!detailObject?.highlight && <>
+        <h4>What I'm most proud of:</h4>
+        <p>{detailObject.highlight}</p>
+      </>}
+    </Box>
+    </Paper>
+  );
+}
+
+const LinksBox = ({ links }) => (
+  <Box sx={linksBoxStyle}>
+    <ButtonGroup variant="text" aria-label="text button group">
+      {links.map((link, index) => {
+        const { name, url } = link;
+        return (
+          <a key={index} href={url} target="_blank">
+            <Button>{name}</Button>
+          </a>
+        );
+      })}
+    </ButtonGroup>
+  </Box>
+)
+
+const TechList = ({ list }) => (
+  <ul>Tech Used:
+    {list.map((tech, index) => {
+      return (        
+        <li key={index}>
+          <i className={tech}></i>
+        </li>
+      );
+    })}
+  </ul>
+)
