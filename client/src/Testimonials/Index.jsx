336395129cc9294409e168795d0e98eb3d61632d
Squall Leonhart <squallpl1983@gmail.com>
2023-04-19 18:07:53 -0400

    Added Testimonials component

--- client/src/Testimonials/Index.jsx
+++ client/src/Testimonials/Index.jsx
@@ -1,9 +1,20 @@
 // Social Proof: testimonials
 
 import React from 'react';
+import { Box, Container, Paper } from '@mui/material';
 
 export default () => {
   return (
-    <></>
+    <Box>
+      <h2>Testimonials</h2>
+      <Container>
+        <Paper elevation={6}>
+          <p></p>
+        </Paper>
+        <Paper elevation={6}>
+          <p></p>
+        </Paper>
+      </Container>
+    </Box>
   )
 }
